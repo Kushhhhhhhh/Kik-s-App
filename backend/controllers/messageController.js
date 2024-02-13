1466af940e03bd36e2610a420311248af9f9fc9f
kush sharma <kush.73888@gmail.com>
2024-02-13 17:23:47 +0530

    Messages Endpoint

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -42,3 +42,28 @@ export const sendMessage = async (req, res) => {
     }
     
 }
+
+export const getMessages = async (req, res) => {
+    try {
+
+       const { id: userToChatId } = req.params;
+       const senderId = req.user._id;
+       const conversation = await Conversation.findOne({
+           participants: {
+               $all: [senderId, userToChatId]
+           }, 
+       }).populate('messages'); 
+
+       if(!conversation) return res.status(404).json([]);
+       
+       const messages = conversation.messages;
+       
+       res.status(200).json(conversation.messages);
+        
+    } catch (error) {
+
+        console.log('Get Message Controller Error: ', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+
+    }
+}
