bac94eca06f396d04b46ed245ca76e341e35dd5e
kush sharma <kush.73888@gmail.com>
2024-02-13 12:36:06 +0530

    Login and Logout Endpoints

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -60,10 +60,43 @@ export const signup = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
-    res.send('login');
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || '');
+
+        if (!user && !isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid Username or Password" });
+        }
+
+        genToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic
+        })
+
+    } catch (error) {
+         // Log the error for debugging purposes
+         console.log('Login Controller Error: ', error);
+        
+         // Respond with a generic error message
+         res.status(500).json({ error: 'Internal Server Error' });
+    }
 }
 
 export const logout = (req, res) => {
-    res.send('logout');
+    try {
+        res.cookie('jwt', '', { maxAge: 0 });
+        res.status(200).json({ message: 'Logged out successfully' });
+    } catch (error) {
+        // Log the error for debugging purposes
+        console.log('Logout Controller Error: ', error);
+        
+        // Respond with a generic error message
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 }
