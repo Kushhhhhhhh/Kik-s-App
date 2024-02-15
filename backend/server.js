19010d6fbb5b142a7565438461078b7c3b9263cb
kush sharma <kush.73888@gmail.com>
2024-02-15 11:45:46 +0530

    Online User by Socket.io

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ import messageRoutes from './routes/messageRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import connectDB from './db/db.js';
 import cookieParser from 'cookie-parser';
+import { app, server } from './socket/socket.js';
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 
@@ -17,11 +17,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
-// app.get('/', (req, res) => {
-//     res.send("Hello Kush");
-// });
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectDB();
     console.log(`Server is running on port ${PORT}`)
 });
